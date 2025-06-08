cc7cedd1598dfcc80d68f0818f1bb71cdab9585b
Ryan Myat Thu <ryanmyatthu@gmail.com>
2025-06-08 22:48:24 +0630

    CRUD ALL

--- YMTDotNetTrainingBatch2.ConsoleApp/AdoDotNetExample.cs
+++ YMTDotNetTrainingBatch2.ConsoleApp/AdoDotNetExample.cs
@@ -23,16 +23,19 @@ namespace YMTDotNetTrainingBatch2.ConsoleApp
         {
             SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
             connection.Open();
-            string query = "SELECT * FROM Tbl_Student";
+            string query = "SELECT * FROM Tbl_Blogs";
             SqlCommand command = new SqlCommand(query, connection);
             SqlDataAdapter adapter = new SqlDataAdapter(command);
             DataTable dataTable = new DataTable();
             adapter.Fill(dataTable);
             connection.Close();
-            for (int i = 0; i < dataTable.Rows.Count; i++)
+           
+            for(int i =0; i < dataTable.Rows.Count; i++)
             {
                 DataRow row = dataTable.Rows[i];
-                Console.WriteLine(row["StudentId"] + "|" + row["StudentNo"] + "|" + row["StudentName"] + "|" + row["Email"]);
+                Console.WriteLine("=> " + row["BlogId"]);
+                Console.WriteLine("=> " + row["BlogTitle"]);
+                Console.WriteLine("=> " + row["BlogAuthor"]);
             }
 
             Console.ReadLine();
@@ -40,36 +43,69 @@ namespace YMTDotNetTrainingBatch2.ConsoleApp
 
         public void Create()
         {
-            string query = @"INSERT INTO [dbo].[Tbl_Student]
-           ([StudentNo]
-           ,[StudentName]
-           ,[DateOfBirth]
-           ,[MobileNo]
-           ,[Email]
-           ,[Gender]
-           ,[Address]
-           ,[FatherName]
-           ,[DeleteFlag])
+            Console.WriteLine("Enter Blog Title: ");
+            string title = Console.ReadLine() + "";
+            Console.WriteLine("Enter Blog Author: ");
+            string author = Console.ReadLine() + "";
+
+            string query = @"INSERT INTO [dbo].[Tbl_Blogs]
+           ([BlogTitle]
+           ,[BlogAuthor])
      VALUES
-           ('S0001'
-           ,'Ye Myat Thu'
-           ,'2006-12-14'
-           ,'09754364261'
-           ,'ryanmyatthu@gmail.com'
-           ,'Male'
-           ,'Ygn MM'
-           ,'U Ye'
-           ,0
-		   )";
+           (@title
+           ,@author)";
 
             SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
             connection.Open();
             SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@title", title);
+            command.Parameters.AddWithValue("@author", author);
             int result = command.ExecuteNonQuery();
             connection.Close();
             Console.WriteLine(result > 0 ? "Insert Success!" : "Insert Failed!");
+        }
 
+        public void Update()
+        {
+            Console.WriteLine("Enter Blog Id to Update: ");
+            string blogId = Console.ReadLine() + "";
+            Console.WriteLine("Enter New Blog Title: ");
+            string title = Console.ReadLine() + "";
+            Console.WriteLine("Enter New Blog Author: ");
+            string author = Console.ReadLine() + "";
 
+            string query = @"UPDATE [dbo].[Tbl_Blogs]
+                            SET [BlogTitle] = @newTitle
+                              ,[BlogAuthor] = @newAuthor
+                             WHERE BlogId = @blogId
+";
+
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@newTitle", title);
+            command.Parameters.AddWithValue("@newAuthor", author);
+            command.Parameters.AddWithValue("@blogId", blogId);
+            int result = command.ExecuteNonQuery();
+            connection.Close();
+            Console.WriteLine(result > 0 ? "Update Success!" : "Update Failed!");
+
+        }
+         
+        public void Delete()
+        {
+            Console.WriteLine("Enter Blog Id to Delete: ");
+            string blogId = Console.ReadLine() + "";
+
+            string query = @"DELETE FROM [dbo].[Tbl_Blogs]
+                             WHERE BlogId = @blogId";
+            SqlConnection connection = new SqlConnection(_sqlConnectionStringBuilder.ConnectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@blogId", blogId);
+            int result = command.ExecuteNonQuery();
+            connection.Close();
+            Console.WriteLine(result > 0 ? "Delete Success!" : "Delete Failed!");
         }
 
 
--- YMTDotNetTrainingBatch2.ConsoleApp/Program.cs
+++ YMTDotNetTrainingBatch2.ConsoleApp/Program.cs
@@ -6,5 +6,4 @@ using YMTDotNetTrainingBatch2.ConsoleApp;
 
 
 AdoDotNetExample adoDotNet = new AdoDotNetExample();
-// adoDotNet.Read();
 adoDotNet.Create();
